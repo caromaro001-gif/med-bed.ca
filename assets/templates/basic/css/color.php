94a378aaf8414ffff012be1ddf72198c09dd2d90
caromaro001-gif <caromaro001@gmail.com>
2026-06-30 12:20:38 -0700

    Add files via upload

---
+++ assets/templates/basic/css/color.php
@@ -0,0 +1,167 @@
+
+*::-webkit-scrollbar-button, *::-webkit-scrollbar-thumb, .scrollToTop, .cmn-btn-active:focus, .cmn-btn-active:hover, .cmn-btn, .border-btn.active, .border-btn:hover, .small-btn::after, .nice-select, .header-bottom-area .navbar-collapse .main-menu li a::after, .header-bottom-area .navbar-collapse .main-menu li .sub-menu li::before, .header-search-form .header-search-btn, .title-border::before, .title-border::after, .title-border-left::after, .banner-slider .banner-pagination span.swiper-pagination-bullet-active::after, .ruddra-next:hover, .ruddra-prev:hover, .choose-slider .swiper-pagination .swiper-pagination-bullet-active, .booking-item .booking-thumb .doc-deg, .booking-item .booking-thumb .fav-btn:hover, .booking-section-two .booking-tag li:hover a, .overview-booking-list .booked::before, .overview-booking-area .clearfix li a.disabled, .faq-wrapper .faq-item.open .right-icon::before, .faq-wrapper .faq-item.open .right-icon::after, .contact-form-area .contact-form .submit-btn, .contact-item-icon i, .blog-details-section .tag-item-wrapper .tag-item:hover, .comments-section .comment-item .comment-content:hover .reply-button i, .footer-form .submit-btn, .date-select, .date-select:focus, .bg-site-color, .card-header, .submit-button, .innercircle, ::selection, .banner-slider .swiper-pagination .swiper-pagination-bullet. .blog-item .blog-thumb .blog-cat {
+background-color: #FF8787;
+}
+
+.pagination .page-item.active .page-link, .pagination .page-item:hover .page-link, .client-content::before, .file-upload-wrapper:before, .pagination .page-item.disabled span {
+background: #FF8787;
+}
+
+.cmn-btn {
+background-color: #FF8787;
+}
+
+.innercircle{
+background-color: #FF8787;
+}
+
+.header-bottom-area .navbar-collapse .main-menu li a::after {
+background-color: #FF8787;
+}
+
+.admin-reply-section{
+background-color: #FF878729;
+}
+
+.outercircle{
+background-color: #FF878790;
+}
+
+.footer-form .submit-btn {
+background-color: #FF8787;
+}
+
+.booking-item .booking-thumb .doc-deg{
+background-color: #FF8787;
+}
+
+.border-btn.active{
+background-color: #FF8787;
+}
+.scrollToTop{
+background-color: #FF8787;
+}
+.copied::after{
+background-color: #FF8787;
+}
+
+.select2-container--default .select2-search--dropdown .select2-search__field:focus {
+border-color: #FF8787 !important;
+}
+
+.select2-container--open .select2-selection.select2-selection--single,
+.select2-container--open .select2-selection.select2-selection--multiple {
+border-color: #FF8787 !important;
+}
+
+.payment-card-title {
+background-color: #FF8787 !important;
+}
+
+.contact-form-area .contact-form .submit-btn {
+background-color: #FF8787 !important;
+}
+
+.card-header {
+background-color: #FF8787 !important;
+}
+
+.blog-details-section .single-popular-item .popular-item-content .title:hover{
+color: #FF8787;
+}
+
+.contact-item-icon i {
+background-color: #FF8787 !important;
+}
+
+.post-share li a {
+color: #FF8787;
+border: 1px solid #FF8787;
+}
+
+.post-share li a:hover{
+background-color: #FF8787 !important;
+border: 1px solid #FF8787;
+}
+
+.payment-system-list {
+--hover-border-color: #FF8787 !important;
+}
+
+
+.payment-system-list.is-scrollable::-webkit-scrollbar-thumb {
+background-color: #FF8787 !important;
+}
+
+.select2-selection--single {
+background: #FF8787 !important;
+}
+
+
+.payment-item:has(.payment-item__radio:checked) .payment-item__check {
+border: 3px solid #FF8787 !important;
+}
+
+.payment-item__check {
+border: 1px solid #FF8787 !important;
+}
+
+.blog-item .blog-thumb .blog-cat{
+background-color: #FF8787;
+}
+.choose-slider .swiper-pagination .swiper-pagination-bullet-active{
+background-color: #FF878790;
+}
+
+.banner-slider .swiper-pagination .swiper-pagination-bullet {
+background-color: #FF8787;
+}
+
+.scrollToTop:hover, .cmn-btn-active, .custom-btn:hover, .header-bottom-area .navbar-collapse .main-menu li.active a, .header-bottom-area .navbar-collapse .main-menu li:hover a, .language-select .nice-select span, .navbar-toggler span, .header-form .header-form-area button, .header-form .header-form-area input[type="button"], .header-form .header-form-area input[type="reset"], .header-form .header-form-area input[type="submit"], .breadcrumb li, .breadcrumb-item.active::before, .banner-slider .banner-pagination span.swiper-pagination-bullet-active, .ruddra-next, .ruddra-prev, .team-content .team-list li i, .client-content .client-icon i, .blog-item .blog-content .title:hover, .call-to-action-area .call-info .call-info-content .title a, .call-to-action-area .mail-info .mail-info-content .title a, .booking-item .booking-content .sub-title, .booking-item .booking-content .booking-list li span, .overview-tab-wrapper .tab-menu li.active, .booking-confirm-area .booking-confirm-list li span, .blog-details-section .category-content li:hover, .privacy-area p a, .ticket-button, .close-button, .text-color, .footer-social li a i:hover, .footer-contact li i, .footer-menus li a i {
+color: #FF8787;
+}
+
+.footer-social li, .footer-social li a:hover, .footer-social li a.active, .cmn--text {
+color: #FF8787 !important;
+}
+
+.scrollToTop, .cmn-btn-active, .title-border-left::before, .ruddra-next, .ruddra-prev, .border-btn {
+border: 1px solid #FF8787;
+}
+
+
+.overview-content .overview-list li .overview-user .before-circle {
+border: 2px solid #FF8787;
+}
+
+.cmn-btn-active:focus, .cmn-btn-active:hover, .cmn-btn:focus, .cmn-btn:hover {
+box-shadow: 0px 15px 20px -8px #FF8787;
+}
+
+.header-bottom-area .navbar-collapse .main-menu li .sub-menu {
+border-left: 3px solid #FF8787;
+}
+
+.search-bar a {
+border: 2px dashed #FF8787;
+}
+
+.language-select .nice-select:after {
+border-color: #FF8787 transparent transparent;
+}
+
+.header-form .header-form-area input {
+border-bottom: 1px solid #FF8787;
+}
+
+.overview-tab-wrapper .tab-menu li.active {
+border-bottom: 2px solid #FF8787;
+}
+
+.pagination .page-item.active .page-link, .pagination .page-item:hover .page-link, .date-select, .date-select:focus {
+border-color: #FF8787;
+}
+
+.payment-item .payment-badge{
+border-right: 60px solid #FF8787;
+}
